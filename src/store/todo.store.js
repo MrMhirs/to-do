19fce740169710cc8f65c967acb1d36b300233df
RB <87038824+MrMhirs@users.noreply.github.com>
2024-01-19 14:47:28 +0100

    todostore start

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,4 +20,43 @@ const initStore = () => {
   console.log("Store initialized");
 };
 
-export default { initStore };
+const loadStore = () => {
+  throw new Error("Not implemented");
+};
+
+/**
+ *
+ * @param {String} description
+ */
+const addTodo = (description) => {
+  throw new Error("Not implemented");
+};
+
+const toggleTodo = (todoId) => {
+  throw new Error("Not implemented");
+};
+
+const deleteTodo = (todoId) => {
+  throw new Error("Not implemented");
+};
+const deleteCompleted = (todoId) => {
+  throw new Error("Not implemented");
+};
+const setFilter = (newFilter = Filters.All) => {
+  throw new Error("Not implemented");
+};
+
+const getCurrentFilter = () => {
+  throw new Error("Not implemented");
+};
+
+export default {
+  addTodo,
+  deleteCompleted,
+  deleteTodo,
+  getCurrentFilter,
+  initStore,
+  loadStore,
+  setFilter,
+  toggleTodo,
+};
