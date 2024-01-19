ea88514fe1973e225143a743eae9a9729a13b1cc
RB <87038824+MrMhirs@users.noreply.github.com>
2024-01-19 16:20:38 +0100

    add/show todos

--- src/todos/app.js
+++ src/todos/app.js
@@ -2,29 +2,39 @@ import html from "./app.html?raw";
 import todoStore from "../store/todo.store";
 import { renderTodos } from "./use-cases/render-todos";
 
-
 const ElementIds = {
   TodoList: ".todo-list",
-}
+  NewTodoInput: "#new-todo-input",
+};
 
 /**
- * 
- * @param {String} elementId 
+ *
+ * @param {String} elementId
  */
 export const App = (elementId) => {
-
-
   const displayTodos = () => {
-
     const todos = todoStore.getTodos(todoStore.getCurrentFilter());
     renderTodos(ElementIds.TodoList, todos);
-  }
+  };
 
-  
+  //App() call
   (() => {
     const app = document.createElement("div");
     app.innerHTML = html;
     document.querySelector(elementId).append(app);
     displayTodos();
   })();
+
+  // Ref HTML
+  const newDescriptionInput = document.querySelector(ElementIds.NewTodoInput);
+
+  //Listeners
+  newDescriptionInput.addEventListener("keyup", (event) => {
+    if (event.keyCode !== 13) return;
+    if (event.target.value.trim().length === 0) return;
+
+    todoStore.addTodo(event.target.value);
+    displayTodos();
+    event.target.value = "";
+  });
 };
